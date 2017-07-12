96c0bddbf99afecacccaf2bb291cd05862b4e560
unos21 <sonukr22@gmail.com>
2017-07-12 22:46:26 +0530

    Update AndroidBoard.mk

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -1,6 +1,6 @@
-LOCAL_PATH := $(call my-dir)
+#LOCAL_PATH := $(call my-dir)
 
 #----------------------------------------------------------------------
 # extra images
 #----------------------------------------------------------------------
-include build/core/generate_extra_images.mk
+#include build/core/generate_extra_images.mk
