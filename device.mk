5790d1907133a81580339f7b389e22e26c0e4b2d
unos21 <sonukr22@gmail.com>
2017-07-14 11:13:13 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+LOCAL_PATH := device/zuk/ham
 
 ifneq ($(QCPATH),)
 $(call inherit-product, $(QCPATH)/common/config/device-vendor.mk)
